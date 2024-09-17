8ab0d913f2c274c4394d4c5f0462972b4f42b092
shanzha <shanzha@gaoding.com>
2024-09-18 01:39:37 +0800

    feat: 增加 iterm2 配置,字体文件

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# vscode start
+set -g fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $fish_user_paths
+# vscode end
